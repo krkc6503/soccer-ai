a236a24d4116043f1a8962ee7677ae298c3df52b
krkc6503 <krkc6503@gmail.com>
2026-07-17 23:38:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ if player != "":
 
     st.write("ID:", row["ID"])
 
-    photo_url = f"https://cdn.sofifa.net/players/{row['ID']}/260006.png"
+    photo_url = f"https://drop-assets.ea.com/images/players/{player_id}.png"
 
     st.image(
     photo_url,
